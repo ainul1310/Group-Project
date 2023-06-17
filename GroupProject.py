8a82e32d11020ca02ace3a2e8786b2cf8d243140
ainul1310 <124066153+ainul1310@users.noreply.github.com>
2023-06-17 17:05:25 +0800

    Update GroupProject.py

--- GroupProject.py
+++ GroupProject.py
@@ -466,26 +466,9 @@ def dashboard3():
         st.write("")
         st.write("")
         st.write("")
-        st.write("")
         st.markdown("<p style='font-size: 21px;text-align: justify;'>WP Kuala Lumpur regularly has the greatest number of stroke cases per year when compared to other states.</p>", unsafe_allow_html=True)
         st.markdown("<p style='font-size: 21px;text-align: justify;'>The surrounding area's bustling and congested environment generate stress and hence raise the probability of stroke.</p>", unsafe_allow_html=True)
         st.markdown("<p style='font-size: 21px;text-align: justify;'>The high cost of living in WP Kuala Lumpur adds to the financial load, magnifying stress levels among residents and likely contributing to the region's increased stroke incidence.</p>", unsafe_allow_html=True)
-
-    st.markdown("---")
-
-    st.markdown(
-        """
-        <div>
-        <p style='font-size: 21px;text-align: justify;'>
-        We may conclude from the information shown in the dashboard that urban regions, particularly WP Kuala 
-        Lumpur, have the largest number of stroke incidents when compared to other locations. This can be ascribed to a 
-        variety of factors, including rising air pollution, dietary trends, high living costs and stress. These factors 
-        combine and lead to the increased total stroke incidence seen in urban areas such as WP Kuala Lumpur.
-        </P>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
 
     st.markdown("---")
 
